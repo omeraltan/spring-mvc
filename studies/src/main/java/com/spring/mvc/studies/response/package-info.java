661417a5bb45c3c192ce4fefe53d2885bd3dddf9
omeraltan <omeraltan66@gmail.com>
2024-02-07 14:22:35 +0300

    13th commit

---
+++ studies/src/main/java/com/spring/mvc/studies/response/package-info.java
@@ -0,0 +1,10 @@
+package com.spring.mvc.studies.response;
+
+/**
+ *
+ *      ResponseEntity:
+ *      ResponseEntity represents the whole HTTP response: "status code", "headers", "body".
+ *      As a result, we can use it to fully configure the HTTP response.
+ *
+ *
+ */
